47a5cd1ab0b7dc63d98c64bb117997483c419bc0
xiang7 <xiang7@purdue.edu>
2014-02-24 11:08:44 -0500

    add file

---
+++ FreqEst.py
@@ -0,0 +1,27 @@
+#estimates frequency of key words given a text
+from acora import AcoraBuilder
+
+
+class FreqEst:
+	
+	def __init__(self,key_list):
+		self._key_list=key_list
+		self._builder = AcoraBuilder(self._key_list)
+		self._ac = self._builder.build()
+		self._table = dict()
+
+	#search a string
+	def search(self, text):
+		for w, f in self._ac.findall(text):
+			if w not in self._table:
+				self._table[w]=1
+			else:
+				self._table[w]+=1
+
+	#search a list
+	def search_list(self,text_list):
+		for l in text_list:
+			self.search(l)
+
+	def get_freq(self):
+		return self._table
