278aac6b3d9433a16d30be2b7045fb16f4751126
xiang7 <xiang7@purdue.edu>
2014-05-31 20:19:03 -0400

    add support for windows

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name='ForumMiner',
 	version='0.0dev',
-	install_requires = ['esmre','nltk'],
+	install_requires = ['esmre','nltk','argparse'],
 	author='Luojie Xiang',
 	test_suite="tests.tests"
 	)
